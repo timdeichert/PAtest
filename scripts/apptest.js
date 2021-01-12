b29816f621c49875263e3647f644c5faa20413b0
Tim Deichert <timdeichert@web.de>
2021-01-12 16:17:53 +0100

    asd

--- scripts/apptest.js
+++ scripts/apptest.js
@@ -5,45 +5,20 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-function startDrag(e) {
-  this.ontouchmove = this.onmspointermove = moveDrag;
-
-  this.ontouchend = this.onmspointerup = function () {
-    this.ontouchmove = this.onmspointermove = null;
-    this.ontouchend = this.onmspointerup = null;
-  }
-
-  var pos = [this.offsetLeft, this.offsetTop];
-  var that = this;
-  var origin = getCoors(e);
-
-  function moveDrag(e) {
-    var currentPos = getCoors(e);
-    var deltaX = currentPos[0] - origin[0];
-    var deltaY = currentPos[1] - origin[1];
-    this.style.left = (pos[0] + deltaX) + 'px';
-    this.style.top = (pos[1] + deltaY) + 'px';
-    return false; // cancels scrolling
-  }
-
-  function getCoors(e) {
-    var coors = [];
-    if (e.targetTouches && e.targetTouches.length) {
-      var thisTouch = e.targetTouches[0];
-      coors[0] = thisTouch.clientX;
-      coors[1] = thisTouch.clientY;
-    } else {
-      coors[0] = e.clientX;
-      coors[1] = e.clientY;
-    }
-    return coors;
-  }
+function getConnection() {
+  return navigator.connection || navigator.mozConnection ||
+    navigator.webkitConnection || navigator.msConnection;
 }
 
-var elements = document.querySelectorAll('.test-element');
-[].forEach.call(elements, function (element) {
-  element.ontouchstart = element.onmspointerdown = startDrag;
-});
+function updateNetworkInfo(info) {
+  document.getElementById('networkType').innerHTML = info.type;
+  document.getElementById('effectiveNetworkType').innerHTML = info.effectiveType;
+  document.getElementById('downlinkMax').innerHTML = info.downlinkMax;
+}
 
-document.ongesturechange = function () {
-  return false;}
+var info = getConnection();
+if (info) {
+  info.onchange = function (event) {
+    updateNetworkInfo(event.target);
+  }
+  updateNetworkInfo(info);}
